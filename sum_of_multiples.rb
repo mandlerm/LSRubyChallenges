f4b0db0ed553afcceef987e482fa1368b3aceb8c
mandlerm <amaya.melissa@gmail.com>
2019-08-22 19:26:21 -0500

    sum of multiples finished

---
+++ sum_of_multiples.rb
@@ -0,0 +1,20 @@
+class SumOfMultiples
+
+  def initialize(*args)
+    @args =  args.empty? ? [3,5] :  args
+  end
+
+  def to(limit)
+    sum = 0
+    (1..limit-1).each do |num|
+      sum += num if @args.any? {|n| num % n == 0  }
+    end
+    sum
+  end
+
+  def self.to(limit)
+    new.to(limit)
+  end
+
+end
+
--- trinary.rb
+++ trinary.rb
@@ -7,6 +7,7 @@ class Trinary
     sum = 0
     raise ArgumentError, "Invalid Entry" if @number =~/[3-9]/
     return @number if @number.to_s.length == 1
+    
     @number.digits.map.with_index do |n, index|
       n * (3 ** index)
     end.sum 
