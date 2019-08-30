8b386ea2cb50aaa90447d0b7cf90ee5197195646
mandlerm <amaya.melissa@gmail.com>
2019-08-29 20:24:53 -0500

    added helper method and case statement

--- beer_song.rb
+++ beer_song.rb
@@ -4,7 +4,19 @@ class BeerSong
   end
 
   def verse(verse_number)
-    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n"
-      "Take one down and pass it around, #{verse_number} bottles of beer on the wall.\n"
+    response = "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{next_num(verse_number)}"
+
+    response
+  end
+
+  def next_num(verse_number)
+    case (verse_number - 1)
+    when (2..99)
+      "#{verse_number - 1} bottles of beer on the wall.\n"
+    when 1
+      "#{1} bottle of beer on the wall.\n"
+    else
+      "no more bottles of beer on the wall.\n"
+    end
   end
 end
