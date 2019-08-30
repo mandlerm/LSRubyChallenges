a2b1324d879400a93894838e29e82dad80c412d4
mandlerm <amaya.melissa@gmail.com>
2019-08-29 20:34:46 -0500

    each single verse request working

--- beer_song.rb
+++ beer_song.rb
@@ -4,19 +4,16 @@ class BeerSong
   end
 
   def verse(verse_number)
-    response = "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{next_num(verse_number)}"
-
-    response
-  end
-
-  def next_num(verse_number)
-    case (verse_number - 1)
-    when (2..99)
-      "#{verse_number - 1} bottles of beer on the wall.\n"
+   case (verse_number)
+    when (3..99)
+      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number - 1} bottles of beer on the wall.\n"
+    when 2
+      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number-1} bottle of beer on the wall.\n"
     when 1
-      "#{1} bottle of beer on the wall.\n"
+      "#{1} bottle of beer on the wall, #{1} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     else
-      "no more bottles of beer on the wall.\n"
+      "No more bottles of beer on the wall, no more bottles of beer.\n" \
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
   end
 end
