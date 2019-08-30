92cc49253c0dc50f22adf789532a3d83c88323ab
mandlerm <amaya.melissa@gmail.com>
2019-08-29 20:09:15 -0500

    class set up and verse method set up

---
+++ beer_song.rb
@@ -0,0 +1,10 @@
+class BeerSong
+  def initialize()
+
+  end
+
+  def verse(verse_number)
+    "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\n"
+      "Take one down and pass it around, #{verse_number} bottles of beer on the wall.\n"
+  end
+end
