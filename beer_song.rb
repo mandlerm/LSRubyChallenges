3acac5e371f0a61afe8bff71043cc30066923db1
mandlerm <amaya.melissa@gmail.com>
2019-08-31 22:08:01 -0500

    rubocop cleanup

--- beer_song.rb
+++ beer_song.rb
@@ -1,24 +1,23 @@
 class BeerSong
-
+  rubocop:disable Metrics/LineLength
   def verse(verse_number)
-    case (verse_number)
+    case verse_number
     when (3..99)
       "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number - 1} bottles of beer on the wall.\n"
     when 2
-      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number-1} bottle of beer on the wall.\n"
+      "#{verse_number} bottles of beer on the wall, #{verse_number} bottles of beer.\nTake one down and pass it around, #{verse_number - 1} bottle of beer on the wall.\n"
     when 1
-      "#{1} bottle of beer on the wall, #{1} bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
+      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
     else
       "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     end
   end
 
-
   def verses(starting_verse, ending_verse)
     full_verse = []
 
-   while starting_verse >= ending_verse
+    while starting_verse >= ending_verse
       full_verse << "#{verse(starting_verse)}\n"
 
       starting_verse -= 1
@@ -29,6 +28,6 @@ class BeerSong
   end
 
   def lyrics
-    verses(99,0)
+    verses(99, 0)
   end
 end
